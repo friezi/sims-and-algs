72e130de0510cedac9c533771cf5728d7a6b6c95
friedemann.zintel <FriedemannZintel@gmx.de>
2020-06-16 21:19:20 +0200

    working on FourierPointGenerator

---
+++ src/main/java/de/zintel/gfx/g3d/FourierPointGenerator.java
@@ -0,0 +1,24 @@
+package de.zintel.gfx.g3d;
+
+import de.zintel.math.Vector3D;
+
+public class FourierPointGenerator extends APointInterpolater3D {
+
+	public FourierPointGenerator(Vector3D start, Vector3D end) {
+		super(start, end);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public StepUnit3D next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
